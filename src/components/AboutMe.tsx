29abbab00a485536dd2b1069058a388c56c744ce
Kirana Friends <shivansh.m@kiranafriends.com>
2024-09-18 17:18:23 +0530

    hiee

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -14,14 +14,10 @@ export default function AboutMe() {
         }
         title="About Me"
         description={
-          <div className="text-lg sm:text-xl">
+          <div>
             I&apos;m a <span className="text-about_me_green">software developer</span> specialized on <span className="text-about_me_green">Web Applications</span>
           </div>
         }
-
-
-
-
       />
       <Stars id="about-me" />
       <div className="@container">
